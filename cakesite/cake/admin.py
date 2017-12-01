c753b2660f241711bf8e3f39e548e258f7e25648
taixiang <taixiang@htyg.com.cn>
2017-12-01 18:15:09 +0800

    admin 排序

--- cakesite/cake/admin.py
+++ cakesite/cake/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import Category, Cake
+from form_utils.widgets import ImageWidget
+from django.db import models
 
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Cake)
 admin.site.site_header = "后台管理"
+
+
+class cakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'size', 'price', 'img1')
+    formfield_overrides = {models.ImageField: {'widget': ImageWidget}}
+
+
+admin.site.register(Cake)
