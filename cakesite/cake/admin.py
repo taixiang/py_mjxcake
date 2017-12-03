ca17ee05c83fab22babaf79e68632c16116c61a7
tx <467220125@qq.com>
2017-12-03 21:30:13 +0800

    图片预览

--- cakesite/cake/admin.py
+++ cakesite/cake/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Category, Cake
 from form_utils.widgets import ImageWidget
 from django.db import models
+from django.utils.safestring import mark_safe
 
 # Register your models here.
 admin.site.register(Category)
@@ -9,8 +10,10 @@ admin.site.site_header = "后台管理"
 
 
 class cakeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'code', 'size', 'price', 'img1')
+    list_display = ('name', 'code', 'size', 'price', 'image')
     formfield_overrides = {models.ImageField: {'widget': ImageWidget}}
+    list_filter = ('category_id',)
+    search_fields = ('name',)
 
 
-admin.site.register(Cake)
+admin.site.register(Cake, cakeAdmin)
--- cakesite/cake/models.py
+++ cakesite/cake/models.py
@@ -23,8 +23,8 @@ class Cake(models.Model):
     size = models.CharField("尺寸", max_length=10, help_text="必填，直接填写对应数字即可，例：填13即为13寸")
     price = models.IntegerField("价格", default=0)
     img1 = models.ImageField("图片1", upload_to="photos/%Y/%m/%d", help_text="必填")
-    img2 = models.ImageField("图片2", upload_to="photos/%Y/%m/%d", blank=True)
-    img3 = models.ImageField("图片3", upload_to="photos/%Y/%m/%d", blank=True)
+    img2 = models.ImageField("图片2", upload_to="photos/%Y/%m/%d", blank=True, null=True)
+    img3 = models.ImageField("图片3", upload_to="photos/%Y/%m/%d", blank=True, null=True)
     label = models.CharField("标签", max_length=100, default="", blank=True)
     order = models.IntegerField("权重值", default=0, help_text="值越大越靠前")
     desc = models.TextField("描述", default="", help_text="每个蛋糕都拥有一段故事", blank=True)
@@ -37,7 +37,7 @@ class Cake(models.Model):
         verbose_name_plural = "蛋糕列表"
 
     def image(self):
-        return '<img src="/static/img/%s" width="50px" height="50px" />' % self.img1
+        return '<img src="/static/img/%s" width="50px" height="50px" />'% self.img1
 
     image.allow_tags = True
 
