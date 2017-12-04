ad728c379aa40fdd8cf2c815463f41723dd3cf7a
tx <467220125@qq.com>
2017-12-04 22:32:41 +0800

    添加默认值

--- cakesite/cake/models.py
+++ cakesite/cake/models.py
@@ -23,8 +23,8 @@ class Cake(models.Model):
     size = models.CharField("尺寸", max_length=10, help_text="必填，直接填写对应数字即可，例：填13即为13寸")
     price = models.IntegerField("价格", default=0)
     img1 = models.ImageField("图片1", upload_to="photos/%Y/%m/%d", help_text="必填")
-    img2 = models.ImageField("图片2", upload_to="photos/%Y/%m/%d", blank=True, null=True)
-    img3 = models.ImageField("图片3", upload_to="photos/%Y/%m/%d", blank=True, null=True)
+    img2 = models.ImageField("图片2", upload_to="photos/%Y/%m/%d", default="photos/2017/12/01/cart_ico1.png", blank=True, null=True)
+    img3 = models.ImageField("图片3", upload_to="photos/%Y/%m/%d", default="photos/2017/12/01/cart_ico1.png", blank=True, null=True)
     label = models.CharField("标签", max_length=100, default="", blank=True)
     order = models.IntegerField("权重值", default=0, help_text="值越大越靠前")
     desc = models.TextField("描述", default="", help_text="每个蛋糕都拥有一段故事", blank=True)
@@ -40,6 +40,7 @@ class Cake(models.Model):
         return '<img src="/static/img/%s" width="50px" height="50px" />'% self.img1
 
     image.allow_tags = True
+
 
     def __str__(self):
         return self.name
